a146d59cc99017d540c5b750dd36a103e823603a
sreedevi R <sreedevir@sreedevis-MacBook-Air.local>
2025-07-28 17:53:17 +0530

    logo changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import "./Layout.css"; // optional for styling
+import "./Layout.css";
+import logo from "../images/logo.png"; // update the path to match your structure
 
 const Layout = () => {
   return (
     <div className="layout">
       {/* Navigation Bar */}
       <nav className="nav-bar">
-        <Link to="/">JS Editor</Link> | <Link to="/docs">Documentation</Link> | <Link to="/about">About</Link>
-      </nav>
+  <div className="logo-container">
+    <img
+      style={{ width: "75px", height: "75px" }}
+      src={logo}
+      alt="JS Visualizer Logo"
+      className="logo"
+    />
+  </div>
+
+  <div className="nav-links">
+    <Link to="/">JS Editor</Link> |{" "}
+    <Link to="/docs">Documentation</Link> |{" "}
+    <Link to="/about">About</Link>
+  </div>
+</nav>
 
       {/* Main content routed via Outlet */}
       <div className="main-content">
